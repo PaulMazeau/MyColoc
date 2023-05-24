62cae85a953cbfac324763fcb6850913a569aeb4
Paul MAZEAU <mazeau.paul@gmail.com>
2023-05-24 11:46:41 +0200

    Correctif bug liste de course

--- screens/ListeDeCourse.tsx
+++ screens/ListeDeCourse.tsx
@@ -1,21 +1,21 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { RootStackParams } from '../App';
 import BackButton from '../components/Reusable/BackButton';
-import CourseCard from '../components/Course/CourseCard';
 import Header from '../components/Reusable/Header';
 import DraggableFlatList, { RenderItemParams } from 'react-native-draggable-flatlist';
 import { CheckBox } from 'react-native-elements';
 import React, { useState } from 'react';
+import { main } from '../constants/Colors';
 
-// Définition du type des propriétés pour le composant ListeDeCourseScreen
 type Props = NativeStackScreenProps<RootStackParams, 'ListeDeCourse'>;
 
 interface Item {
   key: string;
   label: string;
   selected: boolean;
+  isInput?: boolean;
 }
 
 const ListeDeCourseScreen = ({route, navigation}: Props) => {
@@ -23,49 +23,70 @@ const ListeDeCourseScreen = ({route, navigation}: Props) => {
     { key: '1', label: 'Cookie', selected: false },
     { key: '2', label: 'Taboulet', selected: false },
     { key: '3', label: 'Ratio', selected: false },
+    { key: 'input', label: '', selected: false, isInput: true },
   ]);
 
+  const handleItemSelectionToggle = (item: Item, isActive: boolean) => {
+    if (!isActive) {
+      const newData = data.map((dataItem) =>
+        dataItem.key === item.key
+          ? { ...dataItem, selected: !dataItem.selected }
+          : dataItem
+      );
+      setData(newData);
+    }
+  }
+
+  const handleItemSubmission = (event: any) => {
+    const newItem: Item = { key: `${data.length}`, label: event.nativeEvent.text, selected: false };
+    setData(prevData => [...prevData.slice(0, -1), newItem, prevData[prevData.length - 1]]);
+  }
+
   const renderItem = ({ item, drag, isActive }: RenderItemParams<Item>) => {
+    if (item.isInput) {
+      return (
+        <TextInput
+          style={styles.input}
+          placeholder="Ajouter un nouvel élément..."
+          onSubmitEditing={handleItemSubmission}
+          blurOnSubmit={false}
+        />
+      );
+    }
+
     return (
       <TouchableOpacity
-        style={{
-          flexDirection: 'row',
-          height: 50,
-          backgroundColor: isActive ? 'blue' : 'white',
-          alignItems: 'center',
-          justifyContent: 'space-between'
-        }}
-        onLongPress={drag}>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <CheckBox
+        style={[
+          styles.itemContainer,
+          { backgroundColor: isActive ? '#A0D6FF80' : main.BgColor },
+        ]}
+        onLongPress={drag}
+      >
+        <View style={styles.itemView}>
+          <CheckBox
+            checkedIcon='dot-circle-o'
+            uncheckedIcon='circle-o'
             checked={item.selected}
-            onPress={() => {
-              if (!isActive) { // on ne change l'état que si l'élément n'est pas en cours de glissement
-                const newData = data.map((dataItem) =>
-                  dataItem.key === item.key
-                    ? { ...dataItem, selected: !dataItem.selected }
-                    : dataItem
-                );
-                setData(newData);
-              }
-            }}
+            onPress={() => handleItemSelectionToggle(item, isActive)}
           />
-          <Text style={{ color: 'black', fontSize: 18 }}>{item.label}</Text> 
+          <Text style={styles.itemText}>{item.label}</Text> 
         </View>
       </TouchableOpacity>
     );
   };
-   
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  }
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header/>
-      <View style = {styles.Title}>
-        <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center'}}  onPress={() => {
-      navigation.goBack() }}>
-        <BackButton/>
-        <Text style={styles.screenTitle}>{route.params.name}</Text>
+      <View style={styles.titleView}>
+        <TouchableOpacity style={styles.titleTouch} onPress={handleGoBack}>
+          <BackButton/>
+          <Text style={styles.screenTitle}>{route.params.name}</Text>
         </TouchableOpacity>
       </View>
       <DraggableFlatList
@@ -73,6 +94,7 @@ const ListeDeCourseScreen = ({route, navigation}: Props) => {
         renderItem={renderItem}
         keyExtractor={(item) => `draggable-item-${item.key}`}
         onDragEnd={({ data }) => setData(data)}
+        scrollEnabled={data.length > 10}
       />
     </View>
   );
@@ -81,18 +103,41 @@ const ListeDeCourseScreen = ({route, navigation}: Props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: main.BgColor,
   },
   screenTitle: {
     fontSize: 24,
     fontWeight: 'bold',
     marginLeft: 10,
   },
-
-  Title: {
-    flexDirection : 'row', 
-    marginBottom : 15,
+  titleView: {
+    flexDirection: 'row', 
+    marginBottom: 15,
     marginTop: 10
+  },
+  titleTouch: {
+    flexDirection: 'row', 
+    alignItems: 'center'
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    height: 'auto',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  itemView: {
+    flexDirection: 'row', 
+    alignItems: 'center'
+  },
+  itemText: {
+    color: 'black', 
+    fontSize: 18
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
   },
 });
 
