018d5e5d3b6796bd8cc257a22ff905cb11593277
PetiteSirene <ariel.lissak@gmail.com>
2023-05-25 15:24:08 +0200

    BackgroundRemplissage ParticipantCard V1

--- components/MiniJeu/VoteCard.tsx
+++ components/MiniJeu/VoteCard.tsx
@@ -13,8 +13,9 @@ const VoteCard = () => {
                 <Text style={styles.text1}>Vote contre l'incognito</Text>
             </View>
             <View style={styles.secondLign}>
-                <ParticipantCard text="julie" colorBackGround="white" colorText={main.TextColor}  />
-                <ParticipantCard text="julie" colorBackGround="white" colorText={main.TextColor}/>
+                <ParticipantCard text="julie"  />
+                <ParticipantCard text="julie" />
+
             </View>
             <View style={styles.thirdLign}>
                 <Button text={"Voter"} colorBackGround={"#3B41F1"} colorText={'white'}/>
@@ -30,14 +31,15 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         width:windowWidth * 0.9,
         padding:10,
-        flex:0.5
+        flex:0.5,
+        marginBottom:10
         
     },
 
     firstLign: {
         alignItems:'flex-start',
         width:'100%',
-        height:'20%'
+        height:'15%'
     },
 
     secondLign:{
--- components/Reusable/ParticipantCard.tsx
+++ components/Reusable/ParticipantCard.tsx
@@ -1,38 +1,18 @@
 import React from "react";
-import { Text, StyleSheet,TouchableOpacity,Image, View } from "react-native";
-import {main} from './../../constants/Colors'
+import { Text, StyleSheet, TouchableOpacity, Image, View } from "react-native";
+import { main } from './../../constants/Colors';
 
 interface ButtonProps {
     text: string;
-    colorBackGround: string;
-    colorText: string;
     onPress: () => void;
-  }
+}
 
-const ParticipantCard = ({ text, colorBackGround, colorText, onPress }: ButtonProps) => {
-    const styles = StyleSheet.create({
-        global: {
-            backgroundColor: colorBackGround,
-            borderRadius: 10,
-            justifyContent: 'center',
-            alignItems: 'center',
-            height:"100%",
-            marginRight:5,
-            marginLeft:5,
-            width:'20%',
-            borderColor: main.MainColor,
-            borderWidth:2
-        },
-        text: {
-            fontWeight: '600',
-            fontSize: 20,
-            color: colorText
-        }
-    });
-
+const ParticipantCard = ({ text, onPress }: ButtonProps) => {
     return (
         <TouchableOpacity onPress={onPress} style={styles.global}>
-            <View style={styles2.ImageContainer}>
+            <View style={styles.topSection}></View>
+            <View style={styles.bottomSection}></View>
+            <View style={styles.imageContainer}>
                 <Image source={require('../../assets/images/profilIcon.png')} />
             </View>
             <Text style={styles.text}>{text}</Text>
@@ -40,16 +20,51 @@ const ParticipantCard = ({ text, colorBackGround, colorText, onPress }: ButtonPr
     );
 };
 
-const styles2 = StyleSheet.create({
-
-    ImageContainer: {
+const styles = StyleSheet.create({
+    global: {
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 5,
+        marginLeft: 5,
+        width: '20%',
+        borderColor: main.MainColor,
+        borderWidth: 1.5,
+        overflow: 'hidden',  // Cache les parties des vues d'arrière-plan qui dépassent du bouton
+    },
+    topSection: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        height: '80%',  // Couvre 60% du bouton
+        backgroundColor: 'white',
+    },
+    bottomSection: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: '20%',  // Couvre 40% du bouton
+        backgroundColor: 'blue',
+        width:'101%'
+    },
+    text: {
+        fontWeight: '400',
+        fontSize: 20,
+        color: main.TextColor,
+        position: 'absolute',  // Maintient le texte au centre du bouton
+        bottom: '10%',  // Ajustez ces valeurs en fonction de l'endroit où vous voulez que le texte apparaisse
+    },
+    imageContainer: {
         height: 40,
-        width:40,
+        width: 40,
         overflow: 'hidden',
         borderRadius: 20,
-        alignItems:'center',
-      },
-
-})
+        alignItems: 'center',
+        position: 'absolute',  // Maintient l'image au centre du bouton
+        top: '20%',  // Ajustez ces valeurs en fonction de l'endroit où vous voulez que l'image apparaisse
+    },
+});
 
 export default ParticipantCard;
