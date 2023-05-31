370f2fc9a1401faaa846061645a47a48627bd1ad
Paul MAZEAU <mazeau.paul@gmail.com>
2023-05-31 13:18:53 +0200

    BottomSheet Depense

--- components/Depense/AddDepenseBS.tsx
+++ components/Depense/AddDepenseBS.tsx
@@ -10,34 +10,25 @@ const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 const Recurrence = [
-  { label: 'Aucune', value: '1' },
-  { label: '1 jour', value: '2' },
-  { label: '2 jours', value: '3' },
-  { label: '3 jours', value: '4' },
-  { label: '1 semaine', value: '5' },
-  { label: '2 semaines', value: '6' },
-  { label: '1 mois', value: '7' },
-  { label: '2 mois', value: '8' },
-];
+    { label: 'Tout le monde', value: '1' },
+    { label: 'Colocataire 1', value: '2' },
+    { label: 'Colocataire 2', value: '3' },
+    { label: 'Colocataire 3', value: '4' },
+    { label: 'Colocataire 4', value: '5' },
+    { label: 'Colocataire 5', value: '6' },
+  ];
 
-const Notification = [
-  { off: 'Oui', input: '2' },
-  { off: 'Non', input: '1' },
-];
 
-const Rappel = [
-  { rappel: 'Aucun', id: '1' },
-  { rappel: '1 heures', id: '2' },
-  { rappel: '2 heures', id: '3' },
-  { rappel: '1 jour', id: '4' },
-  { rappel: '1 semaine', id: '5' },
-];
 
 const AddDepenseBS = () => {
   const bottomSheetRef = useRef<BottomSheetModal>(null);
-  const [title, setTitle] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
 
+  const [title, onChangeTitre] = React.useState(null);
+const [value, setValue] = useState(null);
+const [day, Day] = React.useState(null);
+const [month, Month] = React.useState(null);
+const [year, Year] = React.useState(null);
   const openBottomSheet = () => {
     bottomSheetRef.current?.present();
   };
@@ -64,7 +55,7 @@ const AddDepenseBS = () => {
   };
 
   return (
-    <View style={styles.container}>
+    <View style={{flex: 1}}>
       <TouchableOpacity onPress={() =>{ Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium); openBottomSheet() }} style={styles.addButton}>
         <AddButton />
       </TouchableOpacity>
@@ -75,68 +66,65 @@ const AddDepenseBS = () => {
         index={0}
         backdropComponent={renderBackdrop}
         enableHandlePanningGesture={true}
-        enableContentPanningGesture={true}
       >
         <View style={styles.contentContainer}>
-          <ScrollView>
-            <Text style={styles.title}>Nouvelle tâche ménagère</Text>
-
-            <View style={styles.inputContainer}>
-              <Text style={styles.subTitle}>Nom</Text>
-              <TextInput
-                style={styles.input}
-                onChangeText={setTitle}
-                value={title}
-                placeholder="Entrer le nom de la tâche"
-              />
-            </View>
-
-            <View style={styles.inputContainer}>
-              <Text style={styles.subTitle}>Date</Text>
-              <View style={styles.inputDate}></View>
-            </View>
-
-            <View style={styles.inputContainer}>
-              <Text style={styles.subTitle}>Récurrence</Text>
-              <View></View>
-            </View>
-
-            <View style={styles.inputContainer}>
-              <View style={styles.groupe}>
-                <View>
-                  <Text style={styles.subTitle}>Notification</Text>
-                  <View></View>
-                </View>
-                <View>
-                  <Text style={styles.subTitle}>Rappel</Text>
-                  <View style={styles.dropdownRappel}></View>
-                </View>
-              </View>
-            </View>
-
-            <View style={styles.inputContainer}>
-              <Text style={styles.subTitle}>Participant</Text>
-              <View style={styles.participant}>
-                <ScrollView
-                  horizontal
-                  showsHorizontalScrollIndicator={false}
-                  contentContainerStyle={styles.participantContainer}
-                  nestedScrollEnabled={true}
-                >
-                  <ParticipantCard />
-                  <ParticipantCard />
-                  <ParticipantCard />
-                  <ParticipantCard />
-                  <ParticipantCard />
-                </ScrollView>
-              </View>
-            </View>
-
-            <TouchableOpacity style={styles.add} onPress={() =>{ Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success); AddDepense() }}>
-              <Plus />
-              <Text style={styles.buttonText}>Ajouter la tâche ménagère</Text>
-            </TouchableOpacity>
-          </ScrollView>
+ 
+ <Text style={styles.Title}>Nouvelle dépense</Text>
+     <View style={styles.depenseTitle}>
+         <Text style={styles.subTitle}>Titre</Text>
+         <TextInput
+                 style={styles.input}
+                 onChangeText={onChangeTitre}
+                 value={title}
+                 placeholder="Entrer le titre"
+                 
+             />
+       </View>
+ 
+       <View style={styles.depenseTitle}>
+         <Text style={styles.subTitle}>Montant</Text>
+         <TextInput
+                 style={styles.input}
+                 onChangeText={setValue}
+                 value={value}
+                 placeholder="Entrer le montant"
+                 keyboardType='numeric'
+             />
+       </View>
+ 
+       <View style={styles.depenseTitle}>
+         <Text style={styles.subTitle}>Payé par :</Text>
+             <View>
+            
+             </View>
+       </View>
+ 
+ 
+ 
+      
+       <View style={styles.depenseTitle}>
+         <Text style={styles.subTitle}>Participant</Text>
+             <View style={styles.participant}>
+                 <ScrollView  
+                     horizontal={true}
+                     showsHorizontalScrollIndicator={false}
+                     contentContainerStyle={{flexGrow: 1}}
+                     keyboardShouldPersistTaps='handled'>
+                         <ParticipantCard/>
+                         <ParticipantCard/>
+                         <ParticipantCard/>
+                         <ParticipantCard/>
+                         <ParticipantCard/>
+                         <ParticipantCard/>
+                 </ScrollView>
+             </View>
+       </View>
+ 
+       <TouchableOpacity style={styles.AddButton} onPress={() => { Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success); AddDepense() }} > 
+       <Plus/>
+       <Text style={styles.buttonText}>Ajouter la dépense</Text>
+       </TouchableOpacity>
+    
         </View>
       </BottomSheetModal>
     </View>
@@ -144,100 +132,131 @@ const AddDepenseBS = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  addButton: {
-    position: 'absolute',
-    bottom: windowHeight * 0.12, // 5% de la hauteur de l'écran
-    right: windowWidth * 0.05, // 5% de la largeur de l'écran
-  },
-  contentContainer: {
-    flex: 1,
-    backgroundColor: 'white',
-    width: '96%',
-    marginHorizontal: '2%',
-    borderRadius: 35,
-    marginBottom: 16,
-    justifyContent: 'space-between',
-  },
-  title: {
-    textAlign: 'center',
-    fontSize: 20,
-    fontWeight: '600',
-    marginBottom: 10,
-  },
-  subTitle: {
-    marginLeft: 16,
-    fontSize: 16,
-    fontWeight: '500',
-  },
-  inputContainer: {
-    marginTop: 15,
-  },
-  input: {
-    height: 44,
-    marginTop: 13,
-    marginLeft: 13,
-    marginRight: 13,
-    borderWidth: 1,
-    borderColor: '#DDDDDD',
-    padding: 10,
-    borderRadius: 14,
-  },
-  inputDate: {
-    height: 44,
-    borderWidth: 1,
-    borderColor: '#DDDDDD',
-    padding: 10,
-    borderRadius: 14,
-    marginTop: 13,
-    marginLeft: 13,
-    marginRight: 13,
-  },
-  groupe: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  dropdownRappel: {
-    marginTop: 13,
-    marginLeft: 13,
-    marginRight: 13,
-    height: 44,
-    borderRadius: 14,
-    padding: 12,
-    elevation: 2,
-    borderWidth: 1,
-    borderColor: '#DDDDDD',
-    width: 145,
-    backgroundColor: 'white',
-  },
-  participant: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginLeft: 13,
-    marginRight: 13,
-    flex: 1,
-  },
-  participantContainer: {
-    flexGrow: 1,
-  },
-  add: {
-    backgroundColor: '#172ACE',
-    height: 56,
-    borderRadius: 13,
-    marginTop: 20,
-    marginLeft: 13,
-    marginRight: 13,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-    marginBottom: 15,
-  },
-  buttonText: {
-    color: 'white',
-    marginLeft: 15,
-  },
+    addButton: {
+        position: 'absolute',
+        bottom: windowHeight * 0.12, // 5% de la hauteur de l'écran
+        right: windowWidth * 0.05, // 5% de la largeur de l'écran
+      },
+    input: {
+        height: 44,
+        marginTop: 13,
+        marginLeft: 13,
+        marginRight: 13,
+        borderWidth: 1,
+        borderColor: '#DDDDDD',
+        padding: 10,
+        borderRadius: 14,
+      },
+
+      inputDate: {
+        height: 44,
+        borderWidth: 1,
+        borderColor: '#DDDDDD',
+        padding: 10,
+        borderRadius: 14,
+        width: 100,
+      },
+
+      date: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginTop: 13,
+      marginLeft: 13,
+      marginRight: 13,
+      },
+
+      Title: {
+        textAlign: 'center',
+        fontSize: 19,
+        fontWeight: '600',
+        marginBottom: 10
+      },
+
+      subTitle: {
+        marginLeft: 16,
+        fontSize: 16,
+        fontWeight: '500'
+      },
+
+      depenseTitle: {
+          marginTop: 15
+      },
+
+      dropdownRecurrence: {
+        marginTop: 13,
+        marginLeft: 13,
+        marginRight: 13,
+        height: 44,
+        backgroundColor: 'white',
+        borderRadius: 14,
+        padding: 12,
+        elevation: 2,
+        borderWidth: 1,
+        borderColor: '#DDDDDD',
+      },
+      
+
+      placeholderStyle: {
+        fontSize: 16,
+      },
+
+      selectedTextStyle: {
+        fontSize: 16,
+      },
+      
+      groupe: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      },
+
+      dropdownRappel: {
+        marginTop: 13,
+        marginLeft: 13,
+        marginRight: 13,
+        height: 44,
+        backgroundColor: 'white',
+        borderRadius: 14,
+        padding: 12,
+        elevation: 2,
+        borderWidth: 1,
+        borderColor: '#DDDDDD',
+        width: 145,
+      },
+
+      participant: {
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginLeft: 13,
+          marginRight: 13,
+      },
+
+      AddButton: {
+        backgroundColor: '#172ACE',
+        height: 56,
+        borderRadius: 13,
+        marginTop: 20,
+        marginLeft: 13,
+        marginRight: 13,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+      },
+
+      buttonText: {
+        color: 'white',
+        marginLeft: 15,
+      },
+
+      contentContainer: {
+        flex: 1,
+        zIndex: 2,
+      },
+
+      OpenBS: {
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+        flex: 1,
+      },
 });
 
 export default AddDepenseBS;
