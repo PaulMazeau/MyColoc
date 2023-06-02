65e9de0b388b974d121b6658d0bad3e7f6bf838a
Paul mazeau <mazeau.paul@gmail.com>
2023-06-02 14:53:20 +0200

    HomePage

--- screens/FirstPage.tsx
+++ screens/FirstPage.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import BlueGradient from '../components/Reusable/BlueGradient'
 import Button from '../components/Reusable/ButtonColor'
@@ -7,28 +7,58 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { AuthStackParams } from '../App'
 
 type Props = NativeStackScreenProps<AuthStackParams, 'FirstPage'>;
+const Logo = require('../assets/images/Icon_Blanc.png');
 
 const FirstPage = ({navigation}: Props) => {
   return (
     <View>
       <BlueGradient height={0.6}/>
-      <CustomButton 
-        title={'S\'inscrire'}  
-        onPress={() => navigation.navigate('SignUp')}
-       />
-      <CustomButton 
-        title={'Se connecter'} 
-        backgroundColor='#172ACE00' 
-        borderColor={'#172ACE'} 
-        borderWidth={2} 
-        color='#172ACE'
-        onPress={() => navigation.navigate('Login')}
-       />
+        <View style={styles.appartementContainer}>
+          <Image source={Logo} style={styles.AppartementImage}/>
+          <Text style={styles.title}>Bievenue sur MyColoc</Text>
+          <Text style={styles.subTitle}>Fini les frictions entre colocataires!</Text>
+        </View>
+          <CustomButton 
+            title={'S\'inscrire'}  
+            onPress={() => navigation.navigate('SignUp')}
+          />
+          <CustomButton 
+            title={'Se connecter'} 
+            backgroundColor='#172ACE00' 
+            borderColor={'#172ACE'} 
+            borderWidth={2} 
+            color='#172ACE'
+            onPress={() => navigation.navigate('Login')}
+          />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  appartementContainer: {
+    position: "absolute",
+    top: "25%",
+    width: "100%",
+    zIndex: 1,
+    alignItems: 'center',
+  },
+  AppartementImage: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain'
+  },
+  title: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 28,
+},
+subTitle: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+},
+
+})
 
 
 export default FirstPage
