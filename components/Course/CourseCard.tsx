6e8b49b04734f897d81365a742196de9733b08bc
Alexandre Wilk <alex@MacBook-Air-de-Alexandre.local>
2023-06-09 14:18:03 +0200

    coursing

--- components/Course/CourseCard.tsx
+++ components/Course/CourseCard.tsx
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import { Shadows } from '../../constants/Shadow';
-import { useContext } from 'react';
-import { CourseContext } from '../../UserContext';
+import { useContext, useState } from 'react';
+import { CourseContext, UserContext } from '../../UserContext';
 import { Colors, Drawer } from 'react-native-ui-lib';
 import * as Haptics from 'expo-haptics';
+import { FB_DB } from '../../firebaseconfig';
+import { deleteDoc, doc } from 'firebase/firestore';
 
 interface Props {
     name: string;
@@ -12,18 +14,19 @@ interface Props {
 }
 //index est l'index de la course en question dans la liste de toutes les courses "courses"
 const CourseCard: React.FC<Props> = ({ name, onPress, index }) => {
-    const handleDelete = async () => {
-        //await deleteDoc(doc(db, "Colocs/"+clcID+"/Courses", courseID)); -> ancien code
-        console.log('delete')
-        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
-      }
+    const [user, setUser] = useContext(UserContext)
+    const[loading, setLoading] = useState(false)
     const [courses, setCourses] = useContext(CourseContext);
     const course = courses[index].data()
-
+    const handleDelete = async () => {
+        setLoading(true)
+        deleteDoc(doc(FB_DB, "Colocs/"+user.colocID+"/Courses", courses[index].id)).then(()=>{setLoading(false)}).catch((err)=>{alert(err.message);setLoading(false)});
+        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
+      }
     return(
         <View style={styles.body}>
         <Drawer
-        rightItems={[{text: 'Supprimer', background: Colors.red30, onPress: () => handleDelete()}]}      
+        rightItems={loading ? [{text: 'Loading', background: Colors.red30}]: [{text: 'Supprimer', background: Colors.red30, onPress: () => handleDelete()}]}      
         style={styles.drawer}
         >
         <View style={styles.sousbody}>
