5447d7bbdc5cbcdcdf2fdaa39ee83aa657527f2c
Ariel <ariel.lissak@gmail.com>
2023-06-15 11:04:39 +0200

    Image football

--- screens/MiniJeu/FootBall/Components/FootBall.tsx
+++ screens/MiniJeu/FootBall/Components/FootBall.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Matter from 'matter-js'
-import { View } from 'react-native'
+import { View, Image  } from 'react-native'
 
 const FootBall = props => {
-    const radius = props.body.bounds.max.x - props.body.position.x // calculate the radius
+    const radius = props.body.bounds.max.x - props.body.position.x 
 
     const xBody = props.body.position.x - radius
     const yBody = props.body.position.y - radius
@@ -11,26 +11,26 @@ const FootBall = props => {
     const color = props.color
 
     return(
-        <View style={{
-            borderWidth:1,
-            borderColor:color,
-            borderStyle:'solid',
-            position:'absolute',
-            left:xBody,
-            top:yBody,
-            width: radius * 2,  // change width to diameter
-            height: radius * 2, // change height to diameter
-            borderRadius: radius // add this line to render a circle
-        }}/>
+        <Image
+            style={{
+                position: 'absolute',
+                left: xBody,
+                top: yBody,
+                width: radius * 2,
+                height: radius * 2,
+                borderRadius: radius
+            }}
+            source={require('./../../../../assets/images/FootBall.png')} 
+        />
     )
 }
 
 export default (world, color, pos, radius) => {
   
-    const initialFootBall = Matter.Bodies.circle( // use circle instead of rectangle
+    const initialFootBall = Matter.Bodies.circle( 
         pos.x,
         pos.y,
-        radius,  // pass radius instead of size
+        radius,  
         {label:'FootBall'}
     )
     Matter.World.add(world, initialFootBall)
