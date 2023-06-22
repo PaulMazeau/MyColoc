b1554267f89b86d61629347dd617d840b1108b28
Ariel <ariel.lissak@gmail.com>
2023-06-22 14:48:24 +0200

    SetUp Basket

---
+++ screens/MiniJeu/BasketBall/SoundManager.tsx
@@ -0,0 +1,45 @@
+import { Audio } from 'expo-av';
+
+const sounds = [
+  {
+    name: 'Hi-Hat',
+    path: require('./../../../assets/sounds/Hi-Hat.mp3'),
+  },
+  {
+    name: 'Drum',
+    path: require('./../../../assets/sounds/Drum-Sticks.mp3'),
+  },
+  {
+    name: 'Bubble',
+    path: require('./../../../assets/sounds/Bubble.mp3'),
+  },
+  // Ajoutez d'autres sons si nécessaire
+];
+
+const loadedSounds = {};
+
+export const loadSounds = async () => {
+  for (const sound of sounds) {
+    const soundObject = new Audio.Sound();
+    try {
+      await soundObject.loadAsync(sound.path);
+      loadedSounds[sound.name] = soundObject;
+    } catch (error) {
+      console.error(`Erreur lors du chargement du son ${sound.name}:`, error);
+    }
+  }
+};
+
+export const playSound = async (soundName) => {
+  const soundObject = loadedSounds[soundName];
+  if (soundObject) {
+    try {
+      await soundObject.unloadAsync(); // décharger le son existant
+      await soundObject.loadAsync(sounds.find(sound => sound.name === soundName).path); // recharger le son
+      await soundObject.playAsync(); // jouer le son
+    } catch (error) {
+      console.error(`Erreur lors de la lecture du son ${soundName}:`, error);
+    }
+  }
+};
+
---
+++ screens/MiniJeu/BasketBall/physics.tsx
@@ -0,0 +1,109 @@
+import Matter from 'matter-js'
+import { Dimensions } from 'react-native';
+import TouchIndicatorEntity from './Components/TouchIndicator';
+import EmojiEntity from './Components/Emoji';
+import { loadSounds, playSound } from './SoundManager';
+
+let start = true;
+let end =false;
+let isEmojiVisible = false;
+let emojiId;
+
+function distance(point1, point2) {
+    let a = point1.x - point2.x;
+    let b = point1.y - point2.y;
+    return Math.sqrt( a*a + b*b );
+}
+
+
+const emojiWin = [
+    require('./../../../assets/images/Biceps_Emoji.png'),
+    require('./../../../assets/images/Clapping_Emoji.png'),
+    require('./../../../assets/images/High_Five_Emoji.png'),
+    require('./../../../assets/images/Thumbs_Up_Emoji.png'),
+];
+
+loadSounds();
+
+let vector = {x:0, y:0};
+
+const Physics = (entities, {touches, time, dispatch}) => {
+    if(start){
+        const touchIndicator = TouchIndicatorEntity({x:0,y:0}, 50);
+        entities.TouchIndicator = touchIndicator;
+        const emoji = EmojiEntity({x:0,y:0}, 50, null);
+        entities.emoji = emoji;
+        playSound('Drum');
+        Matter.Body.setVelocity(entities.FootBall.body, {
+            x: 0,
+            y: -30,
+        })
+        start=false;
+        end = false;
+    }
+
+    let engine = entities.physics.engine;
+    const { height } = Dimensions.get('window');
+
+
+    entities.TouchIndicator.visible = false;
+
+    if(isEmojiVisible){
+        entities.emoji.visible = true;
+        emojiId = setTimeout(() => {
+            entities.emoji.visible = false
+            isEmojiVisible =false;
+        },500)
+    }
+
+    
+
+    touches
+    .filter(t => t.type === 'press')
+    .forEach( t=> {
+        let touchPoint = {x: t.event.pageX, y: t.event.pageY };
+        let ballPosition = entities.FootBall.body.position;
+
+        if (distance({x : touchPoint.x, y : touchPoint.y + 60}, ballPosition) < (entities.FootBall.body.circleRadius * 1.5)) {
+            
+            entities.TouchIndicator.position = touchPoint;
+            entities.TouchIndicator.visible = true;
+
+
+            entities.emoji.image = emojiWin[Math.floor(Math.random() * emojiWin.length)];
+            entities.emoji.position = touchPoint;
+            clearTimeout(emojiId);
+            isEmojiVisible= true;
+
+            vector = {x: ballPosition.x - touchPoint.x, y: ballPosition.y - touchPoint.y};
+            let speed = 30;
+            let length = Math.sqrt(vector.x*vector.x + vector.y*vector.y);
+            vector.x = vector.x / length * speed/2;
+            dispatch({type: 'new-point'})
+            Matter.Body.setVelocity(entities.FootBall.body, {
+                x: vector.x,
+                y: - speed,
+            })
+
+            playSound('Drum')
+        }
+    })
+
+    entities.FootBall.angle += vector.x * 1.75;
+
+    if(entities.FootBall.body.position.y > (height*1.2)){
+        if(end==false){
+            vector = {x:0, y:0};
+            playSound('Hi-Hat');            
+            dispatch({ type: 'game-over' })
+            start=true
+            end = true;
+        }
+    }
+    else{
+        Matter.Engine.update(engine, time.delta)
+    }
+
+    return entities;
+}
+export default Physics;
