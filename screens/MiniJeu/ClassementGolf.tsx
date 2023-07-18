7a0bba993f97666c83fad50b811127d2f6c70b1e
Ariel <ariel.lissak@gmail.com>
2023-07-18 11:32:20 +0200

    style golfClassement

--- screens/MiniJeu/ClassementGolf.tsx
+++ screens/MiniJeu/ClassementGolf.tsx
@@ -72,10 +72,10 @@ const Classement = () => {
                 <Text style={styles.text}>Classement</Text>
             </TouchableOpacity>
             <View style={styles.Classement1}>
-                <ClassementCardPodium scores={scores} name={user.nomColoc} isScrollable={true} imageCorner={require('./../../assets/images/FootBall.png')}/>
+                <ClassementCardPodium scores={scores} name={user.nomColoc} isScrollable={true} imageCorner={require('./../../assets/images/GolfBall.png')}/>
             </View>
             <View style={styles.Classement2}>
-                <ClassementCardScrollable scores={scoresNational} name={"National"} isScrollable={true} imageCorner={require('./../../assets/images/FootBall.png')}/>
+                <ClassementCardScrollable scores={scoresNational} name={"National"} isScrollable={true} imageCorner={require('./../../assets/images/GolfBall.png')}/>
             </View>
         </View>
       </SafeAreaView>
